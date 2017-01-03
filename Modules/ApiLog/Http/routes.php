e9fd9f818e9be716893ed38f15f1b792892813e9
kwdwkiss <kwdwkisscly@163.com>
2017-01-04 00:22:35 +0800

    fix little-1.0

--- Modules/ApiLog/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'apilog', 'namespace' => 'Modules\ApiLog\Http\Controllers'], function()
-{
-    Route::get('/', 'ApiLogController@index');
-});
--- Modules/Model/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'model', 'namespace' => 'Modules\Model\Http\Controllers'], function()
-{
-    Route::get('/', 'ModelController@index');
-});
